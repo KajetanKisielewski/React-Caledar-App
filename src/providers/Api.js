ad8ce608d862a78c49e2896c99169faaf7806201
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2022-01-09 16:03:15 +0100

    preparation of the working environment and creation of a local api

---
+++ src/providers/Api.js
@@ -0,0 +1,47 @@
+export default class Api {
+    constructor() {
+        this.url = 'http://localhost:3005/meetings'
+    }
+
+
+    loadData() {
+        const options = { method: 'GET' };
+        return this._fetch(options, this.url);
+    }
+
+    addData(data) {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'}
+        };
+
+        return this._fetch(options, this.url)
+    }
+
+    delateData(id) {
+        const options = { method: 'DELATE' };
+        return this._fetch(options, this.url, `/${id}`)
+    }
+
+    updateData(data, id) {
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'}
+        };
+
+        return this._fetch(options, this.url, `/${id}`)
+    }
+
+    _fetch(options, path, additionalPath = '') {
+
+        const url = path + additionalPath;
+
+        return fetch(url , options)
+            .then( resp => {
+                if(resp.ok) { return resp.json(); }
+                return Promise.reject(resp)
+            })
+    }
+}
